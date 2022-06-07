d1ba84edc7ea4e5651955ec604788edc4f118ddf
Owl <tiger2225@yandex.ru>
2022-06-07 19:35:14 +0500

    add orders

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,8 +1,10 @@
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
 from django.shortcuts import render
 
 from django.db import transaction
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 from django.urls import reverse_lazy, reverse
@@ -10,6 +12,7 @@ from django.views.generic import ListView, CreateView, UpdateView, DetailView, D
 
 from adminapp.mixin import BaseClassContextMixin
 from basket.models import Basket
+from mainapp.models import Product
 from ordersapp.forms import OrderItemsForm
 from ordersapp.models import Order, OrderItem
 
@@ -112,3 +115,27 @@ def order_forming_complete(request, pk):
     order.status = Order.SEND_TO_PROCESSED
     order.save()
     return HttpResponseRedirect(reverse('orders:list'))
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.get(pk=pk)
+        if product:
+            return JsonResponse({'price': product.price})
+        return JsonResponse({'price': 0})
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, instance, **kwargs):
+    if instance.pk:
+        item = instance.get_item(int(instance.pk))
+        instance.product.quantity -= instance.quantity - item
+    else:
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+        instance.product.quantity += instance.quantity
+        instance.save()
